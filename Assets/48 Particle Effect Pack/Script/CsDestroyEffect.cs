66abb1a79db045e99fa4481e9298c2cf2d10d8df
ookami1204 <corocorobox12@gmail.com>
2022-09-20 18:10:53 +0900

    firstcommit

---
+++ Assets/48 Particle Effect Pack/Script/CsDestroyEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CsDestroyEffect : MonoBehaviour 
+{
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.C))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
